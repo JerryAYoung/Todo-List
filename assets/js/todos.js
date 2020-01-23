fbb29711ec79e08cb0c06279123212dbc30939f5
Jerry A Young <jerryandrewyoung@gmail.com>
2020-01-22 20:01:15 -0800

    Added logic for checking off todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,16 @@
 // Check off specific todos by clicking
 
 $("li").click(function(){
-	$(this).css("colo", "gray")
-})
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		$(this).css({
+			color: "black",
+			textDecoration: "none"
+		});
+	}
+	else {
+		$(this).css({
+			color: "grey",
+			textDecoration: "line-through"
+		});
+	}
+});
